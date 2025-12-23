185bb6473c5fbf63ceb47712a134598fb33a4486
XDead27 <intricate.layers@protonmail.com>
2025-12-23 14:05:21 +0100

    change telescope to master branch

--- lua/plugin_specs.lua
+++ lua/plugin_specs.lua
@@ -143,7 +143,7 @@ local plugin_specs = {
 
   {
     "nvim-telescope/telescope.nvim",
-    branch = '0.1.x',
+    branch = 'master',
     dependencies = {
       'nvim-lua/plenary.nvim',
       -- Fuzzy Finder Algorithm which requires local dependencies to be built.
