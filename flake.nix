1a8ec9bb841f19976cdb3f5d2a033ff0e0240096
XDead27 <intricate.layers@protonmail.com>
2026-04-24 15:54:00 +0200

    change to new name

--- flake.nix
+++ flake.nix
@@ -96,7 +96,7 @@
           bacon
           bash-language-server
           clang-tools
-          claude-code-acp
+          claude-agent-acp
           fd
           lua-language-server
           nix-doc
